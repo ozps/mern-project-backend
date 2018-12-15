7472004e19c9967282add3b6ef12813a63a730a8
Panjapol Sanguanpanich <panjaphol@hotmail.com>
2018-12-15 12:15:02 +0700

    Update Search Function

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -35,22 +35,57 @@ router.post('/add_new_item', async (req, res) => {
     res.status(200).send({ message: 'Success' })
 })
 
-router.post('/remove_item', async (req, res) => {
+router.post('/remove_item', (req, res) => {
     console.log(req.body)
     Item.findByIdAndDelete(
         mongoose.Types.ObjectId(req.body.id),
         (error, item) => {
             if (error) throw error
             console.log(item)
-            res.status(200).send(item)
+            // res.status(200).send(item)
         }
     )
-
     console.log(item)
     res.status(200).send({ message: 'Success' })
 })
-//app.use('/', express.static(join(_dirname, '../public')))
 
-//updateItem(Optional)
+router.post('/update_price_of_item', (req, res) => {
+    //console.log(req.body)
+    Item.findByIdAndUpdate(
+        mongoose.Types.ObjectId(req.body.id),
+        { itemPrice: req.body.price },
+        (error, item) => {
+            if (error) throw error
+            console.log(item)
+            // res.status(200).send(item)
+        }
+    )
+    console.log(item)
+    res.status(200).send({ message: 'Success' })
+})
+
+router.post('/search_items', (req, res) => {
+    var query = req.body.query.trim()
+    var results = []
+    Item.find({}, async (error, items) => {
+        if (error) throw error
+        // console.log(items)
+        for (let x in items) {
+            // console.log(items[x])
+            // console.log(items[x].itemName)
+            // console.log(items[x].itemDesc)
+            desc = items[x].itemDesc !== undefined ? items[x].itemDesc : ''
+            name = items[x].itemName !== undefined ? items[x].itemName : ''
+            if (desc.includes(query) || name.includes(query)) {
+                results.push(items[x])
+                console.log(results)
+            }
+        }
+        await console.log(results)
+        await res.status(200).send(results)
+    })
+})
+
+//app.use('/', express.static(join(_dirname, '../public')))
 
 module.exports = router
